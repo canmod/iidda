0071697844956d3ff4611b8a94e5c2c22fcfcbd2
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-08-14 10:57:04 -0400

    header comment removal

--- R/remove-header-comments.R
+++ R/remove-header-comments.R
@@ -1,6 +1,13 @@
-scripts = Sys.glob("pipelines/**/prep-scripts/*.R") |> lapply(readLines)
-scripts[[20]] |> head()
+paths = Sys.glob("pipelines/**/prep-scripts/*.R")
+scripts = lapply(paths, readLines)
 locations = (scripts
   |> lapply(grep, pattern = "This script has been automatically modified")
   |> vapply(\(x) x[1L], numeric(1L))
 )
+
+for (i in seq_along(paths)) {
+  if (!is.na(locations[i])) {
+    lines_to_remove = locations[i] + (-1:2)
+    writeLines(scripts[[i]][-lines_to_remove], paths[i])
+  }
+}
